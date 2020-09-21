aab8bea438d67fc8c5b86a5b9d3726cd58ec21c8
mahou2399 <48819469+mahou2399@users.noreply.github.com>
2020-09-21 19:28:19 +0700

    se lab7.2 jpa

--- src/main/java/csku/atm/service/CustomerService.java
+++ src/main/java/csku/atm/service/CustomerService.java
@@ -8,6 +8,7 @@ import csku.atm.model.Customer;
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class CustomerService {
@@ -25,11 +26,12 @@ public class CustomerService {
 
     public Customer findCustomer(int id) {
         try {
-            return repository.findById(id);
-        } catch (EmptyResultDataAccessException e) {
+            return repository.findById(id).get();
+        } catch (NoSuchElementException e) {
             return null;
         }
     }
+
 
     public List<Customer> getCustomers() {
         return repository.findAll();
